2de175fea2a8b279c642d3352012a6fa8018aa26
Abubakri Sherif <saykeed@gmail.com>
2022-12-05 10:13:31 +0100

    minor adjustment

--- src/views/auth/reset.vue
+++ src/views/auth/reset.vue
@@ -2,7 +2,7 @@
 	<auth-layout>
 		<div class="flex flex-col gap-8 py-6 md:flex-row md:items-end">
 			<img src="@/assets/img/illustrations/verify.svg" class="w-1/2 hidden lg:flex" alt="">
-			<FormCard class="lg:mb-[50px]">
+			<FormCard class="lg:mb-[150px]">
 				<div class="flex flex-col gap-2">
 					<h4 class="heading5 md:heading4 text-center text-boldText">Reset Password</h4>
 					<p class="small-text text-[#404040] text-center md:w-[70%] mx-auto">Please enter the OTP sent to you email <span class="text-primary">abisoye@eisoft.com.ng</span> to continue</p>
