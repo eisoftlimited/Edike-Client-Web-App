f562f87f1aaf87aa1473212ce84a6c528f693b1c
Abubakri Sherif <saykeed@gmail.com>
2022-12-24 10:07:43 +0100

    updated

--- src/components/profile/tabs/Details.vue
+++ src/components/profile/tabs/Details.vue
@@ -5,7 +5,7 @@
 				<h3 class="text-[22px] font-medium">Personal Details</h3>
 				<p class="small-text text-[#5F6165]">Update your photo and personal details here.</p>
 			</div>
-			<form class="flex flex-col gap-6" @submit.prevent="updateUser">
+			<form class="flex flex-col gap-6" @submit.prevent="updateUserData">
 				<TextInput input-type="text" :if-required="true" label="First Name" place-holder="" v-model="profile_firstName"/>
 				<TextInput input-type="text" :if-required="true" label="Last Name" place-holder="" v-model="profile_lastName"/>
 				<TextInput input-type="email" :if-required="true" label="Email" place-holder="" v-model="profile_email"/>
@@ -23,7 +23,7 @@ import PhoneInput from '../../utils/phoneInput.vue';
 import { ref, computed } from 'vue';
 import { useUser } from '../../../composables/UserController';
 
-const { firstName, lastName, email, phone, updateUser } = useUser()
+const { firstName, lastName, email, phone, updateUser, updateUserData } = useUser()
 const profile_firstName = ref(firstName.value)
 const profile_lastName = ref(lastName.value)
 const profile_email = ref(email.value)
